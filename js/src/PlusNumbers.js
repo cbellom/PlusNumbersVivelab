4191c2eb066bdf68311a8d49b194d87c90e129c6
idea-PC\Shergat <cbellom@unal.edu.co>
2016-12-02 23:06:18 -0500

    Plus numbers and validate plus operation

--- js/src/PlusNumbers.js
+++ js/src/PlusNumbers.js
@@ -1,7 +1,14 @@
 var PlusNumbers = {
 
   plus: function(x , y){
+    if(this.isValidPlus(x, y))
+      return x + y;
+
     return null;
+  },
+
+  isValidPlus: function(x, y){
+    return !(x < 0 || y < 0 || (x+y) >999);
   }
 
 };
--- spec/helpers/TestPlusNumbers.js
+++ spec/helpers/TestPlusNumbers.js
@@ -4,8 +4,25 @@ describe("Plus Numbers", function() {
 
   var PlusNumbers = require('../../js/src/PlusNumbers');
 
-  it("should be able plus numbers", function() {
+  it("should be defined plus numbers", function() {
     expect(PlusNumbers.plus(0,0)).toBeDefined();
+  });
+
+  it("should be able to plus numbers", function() {
+    expect(PlusNumbers.plus(0,0)).toEqual(0);
+    expect(PlusNumbers.plus(1,0)).toEqual(1);
+    expect(PlusNumbers.plus(100,100)).toEqual(200);
+    expect(PlusNumbers.plus(999,1)).toEqual(null);
+    expect(PlusNumbers.plus(-1,-1)).toEqual(null)
+  });
+
+  it("should validate numbers", function() {
+    expect(PlusNumbers.isValidPlus(0,0)).toEqual(true);
+    expect(PlusNumbers.isValidPlus(1,0)).toEqual(true);
+    expect(PlusNumbers.isValidPlus(100,100)).toEqual(true);
+    expect(PlusNumbers.isValidPlus(999,0)).toEqual(true);
+    expect(PlusNumbers.isValidPlus(999,1)).toEqual(false);
+    expect(PlusNumbers.isValidPlus(-1,-1)).toEqual(false)
   });
 
 });
